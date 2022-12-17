ca084d2b28fb93971a92ac90cf69145cec252b61
maxrainy <maxrainy@gmail.com>
2022-12-18 00:00:10 +0300

    Update filters.py

--- News/filters.py
+++ News/filters.py
@@ -6,12 +6,6 @@ from .models import Post
 # Создаем свой набор фильтров для модели Post.
 class PostFilter(FilterSet):
     class Meta:
-        filter_date = DateFilter(
-            field_name='creation_type',
-            widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-            lookup_expr='gt',
-            label='Start Date',
-        )
         model = Post
         fields = {
 
